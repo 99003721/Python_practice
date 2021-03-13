c36a4b83cc42bb5434c9f4f8be8cf032452b8856
99003721 <78872643+99003721@users.noreply.github.com>
2021-03-13 17:36:57 +0530

    Initial Commit using Pycharm on windows

---
+++ 08_className.py
@@ -0,0 +1,10 @@
+class Person:
+ def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+myDetails = Person("Himanshu", 23)
+
+print("Hello my name is " , myDetails.name + " and age is " , myDetails.age)
+
+# Output : Hello my name is  Himanshu and age is  23
